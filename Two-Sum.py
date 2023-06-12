a3a214761430c608e2224dab215276ba398734d3
Aima Alakhume <71132341+AimaAlakhume@users.noreply.github.com>
2023-06-12 20:50:09 +0100

    Create Two-Sum.py

---
+++ Two-Sum.py
@@ -0,0 +1,23 @@
+def twoSum(nums, target): #nums = N, target = T
+	seen = dict() #O(N) space
+	otherNum = None
+	found = False
+	output = []
+
+	while not found:
+		for i in range(len(nums)): #0(N) time
+			otherNum = target - nums[i]
+			if otherNum not in seen:
+				seen[nums[i]] = i
+			else:
+				output.append(i)
+				output.append(seen[otherNum])
+				found = True
+	
+	return output
+
+nums = [3, 3]
+target = 6
+print(twoSum(nums, target))
+
+#Time = O(N), Space = O(N)
